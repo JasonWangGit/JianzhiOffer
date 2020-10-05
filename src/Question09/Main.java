6228edf15aabd71e1aac0ccb831e3048c24a1357
Jason <wangjiachuan@vip.163.com>
2020-10-05 17:47:03 +1100

    Version 1 for question 09.

---
+++ src/Question09/Main.java
@@ -0,0 +1,52 @@
+package Question09;
+
+public class Main {
+	// main() just for test, if you wanna submit on leetcode, reference to the
+	// function() beneath main()
+	public static void main(String[] args) {
+		CQueue cQueue = new CQueue();
+		System.out.println(cQueue.deleteHead());
+		cQueue.appendTail(97);
+		System.out.println(cQueue.deleteHead());
+		System.out.println(cQueue.deleteHead());
+		System.out.println(cQueue.deleteHead());
+		System.out.println(cQueue.deleteHead());
+		cQueue.appendTail(15);
+		System.out.println(cQueue.deleteHead());
+		cQueue.appendTail(1);
+		cQueue.appendTail(43);
+		System.out.println(cQueue.deleteHead());
+		System.out.println(cQueue.deleteHead());
+		System.out.println(cQueue.deleteHead());
+		cQueue.appendTail(18);
+		System.out.println(cQueue.deleteHead());
+		System.out.println(cQueue.deleteHead());
+		System.out.println(cQueue.deleteHead());
+		System.out.println(cQueue.deleteHead());
+		cQueue.appendTail(36);
+		cQueue.appendTail(69);
+		cQueue.appendTail(21);
+		cQueue.appendTail(91);
+		System.out.println(cQueue.deleteHead());
+		System.out.println(cQueue.deleteHead());
+		cQueue.appendTail(22);
+		cQueue.appendTail(40);
+		System.out.println(cQueue.deleteHead());
+		System.out.println(cQueue.deleteHead());
+		System.out.println(cQueue.deleteHead());
+		cQueue.appendTail(81);
+		cQueue.appendTail(65);
+		System.out.println(cQueue.deleteHead());
+		cQueue.appendTail(77);
+		System.out.println(cQueue.deleteHead());
+		cQueue.appendTail(63);
+		cQueue.appendTail(96);
+		cQueue.appendTail(5);
+		System.out.println(cQueue.deleteHead());
+		System.out.println(cQueue.deleteHead());
+		cQueue.appendTail(35);
+		cQueue.appendTail(90);
+		System.out.println(cQueue.deleteHead());
+		System.out.println(cQueue.deleteHead());
+	}
+}

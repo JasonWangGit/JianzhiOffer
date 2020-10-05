f3c1cff96aff3a7fd44bdb28b01695e1d7fa4744
Jason <wangjiachuan@vip.163.com>
2020-10-05 17:05:12 +1100

    Version 1 for question 03.

---
+++ src/Question03/Solution.java
@@ -0,0 +1,34 @@
+package Question03;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
+
+public class Solution {
+	// main() just for test, if you wanna submit on leetcode, reference to the
+	// function() beneath main()
+	public static void main(String[] args) {
+		int[] array = {2, 3, 1, 0, 2, 5, 3};
+		System.out.println(findRepeatNumber(array));
+	}
+
+	public static int findRepeatNumber(int[] nums) {
+		Map<Integer, Boolean> map = new HashMap<>();
+		int n = 0;
+		int temp = 0;
+		for(int i : nums) {
+			if(!map.containsKey(i)) {
+				map.put(i, false);
+			} else {
+				if(!map.get(i)) {
+					n++;
+					map.replace(i, true);
+					temp =  i;
+					break;
+				}
+			}
+		}
+		return temp;
+    }
+}

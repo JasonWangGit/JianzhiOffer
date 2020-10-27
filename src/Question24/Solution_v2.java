fe206ef6e21f6616dd9d9391e2b4a899b124bd5e
unknown <wangjiachuan@vip.163.com>
2020-10-27 17:06:08 +0800

    Version 2 for question 24.

---
+++ src/Question24/Solution_v2.java
@@ -0,0 +1,45 @@
+package Question24;
+
+import java.awt.List;
+
+// https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/
+
+public class Solution_v2 {
+	public static void display(ListNode head) {
+		if (head == null) {
+			return;
+		}
+		ListNode current = head;
+		while (current.next != null) {
+			System.out.print(current.val + "->");
+			current = current.next;
+		}
+		System.out.println(current.val);
+	}
+	// main() just for test, if you wanna submit on leetcode, reference to the
+	// function() beneath main()
+	public static void main(String[] args) {
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		head.next.next.next.next = new ListNode(5);
+		display(reverseList(head));
+	}
+	
+	public static ListNode reverseList(ListNode head) {
+		if(head == null)
+			return null;
+		ListNode last = null;
+		ListNode current = head;
+		ListNode next = head.next;
+		while(next != null) {
+			current.next = last;
+			last = current;
+			current = next;
+			next = next.next;
+		}
+		current.next = last;
+		return current;
+    }
+}
